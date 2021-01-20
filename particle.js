dfb504acf8418320b9584a120d93d1b6b8ced40a
Aman16Feb2007 <72873498+Aman16Feb2007@users.noreply.github.com>
2021-01-20 19:52:29 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,26 @@
+class Particle{
+    constructor(x,y){
+        var options={
+            restitution : 0.3
+        }
+        this.body = Bodies.circle(x,y,10,options); 
+        this.radius = 10;
+        this.color = color(random(0,225),random(50,200),random(100,250));
+        World.add(world, this.body);
+    }
+
+    /*Score(){
+        if(this.body){
+            Score = Score +10
+        }
+    }*/
+
+    display(){
+        var pos = this.body.position;
+        push();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y, this.radius,this.radius);
+        pop();
+    }
+}
